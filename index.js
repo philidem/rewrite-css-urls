52ae2481f9a1c16c1491a6d2b535fa72057d6c58
Phillip Gates-Idem <phillip.idem@gmail.com>
2016-03-10 14:24:01 -0500

    Modifying JavaScript API (breaking changes)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const tokenizerRegExp = /url\(\s*[\"\']?([^\"\'\)]+)[\"\']?\s*\)|\@import\s+\"([^\"]+)\"|\/\*|\*\/|\\"/g;
+var tokenizerRegExp = /url\(\s*[\"\']?([^\"\'\)]+)[\"\']?\s*\)|\@import\s+\"([^\"]+)\"|\/\*|\*\/|\\"/g;
 
 // Matching groups:
 // 1) url(...)
@@ -9,23 +9,35 @@ const tokenizerRegExp = /url\(\s*[\"\']?([^\"\'\)]+)[\"\']?\s*\)|\@import\s+\"([
 // 4) */
 // 5) "
 
-function Replacement(url, start, end) {
+var Type = exports.Type = {
+    'IMPORT_URL': 1,
+    'URL': 2
+};
+
+function UrlRef(url, start, end, type) {
     this.url = url;
     this.start = start;
     this.end = end;
+    this.type = type;
 }
 
-exports.process = function(code, options) {
-    var replaceImportUrl = options.replaceImportUrl;
-    var replaceAssetUrl = options.replaceAssetUrl;
-
+function findUrlReferences(code, options) {
     var matches;
     var inMultiLineComment = false;
     var inString = false;
     var urlStartPos;
     var match;
-    var replacementUrl;
-    var replacements = [];
+    var urlReferences = [];
+    var urlRef;
+    var forEachFn;
+
+    if (options) {
+        if (options.constructor === Function) {
+            forEachFn = options;
+        } else {
+            forEachFn = options.forEach;
+        }
+    }
 
     while((matches = tokenizerRegExp.exec(code)) != null) {
         var url;
@@ -44,40 +56,75 @@ exports.process = function(code, options) {
             inString = true;
         } else if ((url = matches[1])) {
             // ignore "data:" URLs
-            if ((url.indexOf('data:') !== 0) &&
-                replaceAssetUrl && (replacementUrl = replaceAssetUrl(url))) {
+            if (url.indexOf('data:') !== 0) {
                 match = matches[0];
                 // Found:
                 //  url("<url>")
                 //  url('<url>')
                 //  url(<url>)
                 urlStartPos = matches.index + match.indexOf(url);
-                replacements.push(new Replacement(
-                    replacementUrl,
+
+                forEachFn(new UrlRef(
+                    url,
                     urlStartPos,
-                    urlStartPos + url.length));
+                    urlStartPos + url.length,
+                    Type.URL));
+
+                urlReferences.push(urlRef);
             }
         } else if ((url = matches[2])) {
-            if (replaceImportUrl && (replacementUrl = replaceImportUrl(url))) {
-                match = matches[0];
+            match = matches[0];
 
-                // Found: @import "<url>"
-                urlStartPos = matches.index + match.indexOf(url);
-                replacements.push(new Replacement(
-                    replacementUrl,
-                    urlStartPos,
-                    urlStartPos + url.length));
-            }
+            // Found: @import "<url>"
+            urlStartPos = matches.index + match.indexOf(url);
+
+            forEachFn(new UrlRef(
+                url,
+                urlStartPos,
+                urlStartPos + url.length,
+                Type.IMPORT_URL
+            ));
         }
     }
+}
 
-    var i = replacements.length;
+exports.findUrlReferences = findUrlReferences;
+
+exports.process = function(code, options) {
+    var replaceImportUrl = options.replaceImportUrl;
+    var replaceAssetUrl = options.replaceAssetUrl;
+    var replacementUrl;
+
+    var urlReferences = options.urlReferences;
+
+    if (!urlReferences) {
+        urlReferences = [];
+        findUrlReferences(code, function(urlRef) {
+            urlReferences.push(urlRef);
+        });
+    }
+
+    var i = urlReferences.length;
+
+    // Always perform replacements starting from the end of the
+    // string so that character ranges don't change!!!
     while(--i >= 0) {
-        var replacement = replacements[i];
-        code =
-            code.substring(0, replacement.start) +
-            replacement.url +
-            code.substring(replacement.end);
+        var urlRef = urlReferences[i];
+        if (urlRef.type === Type.IMPORT_URL) {
+            if (!replaceImportUrl || !(replacementUrl = replaceImportUrl(urlRef))) {
+                continue;
+            }
+        } else {
+            if (!replaceAssetUrl || !(replacementUrl = replaceAssetUrl(urlRef))) {
+                continue;
+            }
+        }
+
+        if (replacementUrl) {
+            code = code.substring(0, urlRef.start) +
+                   replacementUrl +
+                   code.substring(urlRef.end);
+        }
     }
 
     return code;
